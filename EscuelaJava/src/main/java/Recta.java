fb3d8856aa191f18a2371c729743e32376bef37b
ignacio <ignaciorobledounq@gmail.com>
2019-12-02 09:33:37 -0300

    Igualdad de recta y constructor con coordenadas

--- EscuelaJava/src/main/java/Recta.java
+++ EscuelaJava/src/main/java/Recta.java
@@ -1,22 +1,35 @@
 public class Recta {
 
     private Punto punto;
-    private Punto otro;
+    private Double pendiente;
 
     Recta(Punto punto, Punto otro) {
         this.punto = punto;
-        this.otro = otro;
+        pendiente = (otro.getY() - punto.getY()) / (otro.getX() - punto.getX());
+    }
+
+    Recta(Double pendiente, Punto punto) {
+        this.punto = punto;
+        this.pendiente = pendiente;
     }
 
     Double pendiente() {
-        return (otro.getY() - punto.getY()) / (otro.getX() - punto.getX());
-    }
-
-    public String toString() {
-        return punto.toString() + " , " + otro.toString();
+        return pendiente;
     }
 
     public Boolean esPunto(Punto p) {
         return pendiente().equals((p.getY() - punto.getY()) / (p.getX() - punto.getX()));
+    }
+
+    public Boolean esPunto(Double x, Double y) {
+        return esPunto(new Punto(x, y));
+    }
+
+    public Boolean equals(Recta recta) {
+        return esPunto(recta.punto()) && pendiente().equals(recta.pendiente());
+    }
+
+    private Punto punto() {
+        return punto;
     }
 }
